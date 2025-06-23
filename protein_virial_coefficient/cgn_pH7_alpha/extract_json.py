9d8ddc49fddbcf78cd4d4c65582fcefa89fead1e
IVinterbladh <isabel.vinterbladh@teokem.lu.se>
2025-06-23 14:10:00 +0200

    fixed last ruff error

--- protein_virial_coefficient/cgn_pH7_alpha/extract_json.py
+++ protein_virial_coefficient/cgn_pH7_alpha/extract_json.py
@@ -2,11 +2,11 @@ import json
 from scipy.constants import Avogadro as NA
 
 
-I=["0.005", "0.010", "0.015", "0.020", "0.030", "0.040", "0.060", "0.080", "0.1"]
+ion_strength=["0.005", "0.010", "0.015", "0.020", "0.030", "0.040", "0.060", "0.080", "0.1"]
 MW=25273.65 # g/mol
 ML_PER_ANGSTROM3 = 1e-24
 
-for molarity in I:
+for molarity in ion_strength:
       filename = f"pmf_{molarity}.json"
       #if not os.path.isfile(filename):
       #    continue
