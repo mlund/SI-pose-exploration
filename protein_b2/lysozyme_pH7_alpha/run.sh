a3c0db17c3e3c26e373c9c7e33fddb95d094cc82
mlund <mlund@localhost>
2025-07-01 21:23:57 +0200

    Use 4 subdivisions for lysozyme

--- protein_b2/lysozyme_pH7_alpha/run.sh
+++ protein_b2/lysozyme_pH7_alpha/run.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
 for molarity in 0.005 0.010 0.015 0.020 0.030 0.040 0.060 0.080 0.1; do
+sbatch <<EOF
+#!/bin/sh
+#SBATCH -J duello_${molarity}
+#SBATCH --output=duello_${molarity}.out
+#SBATCH --error=duello_${molarity}.err
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=48
+#SBATCH -A lu2025-2-42
 RUST_LOG="Debug" $HOME/.cargo/bin/duello \
     scan \
     -1 4lzt.xyz \
     -2 4lzt.xyz \
     --rmin 25 --rmax 100 --dr 1.0 \
     --top topology.yaml \
-    --resolution 0.5 \
+    --resolution 0.25 \
     --cutoff 1000 \
     --molarity ${molarity} \
     --temperature 300 \
     --pmf pmf_${molarity}.dat
+EOF
 done
