e8d1c211c9fa40c86fa2302b91f49c247e0e3fe1
mlund <mlund@localhost>
2025-07-01 14:33:02 +0200

    Use 4 subdivisions for protein B2

--- protein_virial_coefficient/cgn_pH7_alpha/run.sh
+++ protein_virial_coefficient/cgn_pH7_alpha/run.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
 for molarity in 0.005 0.010 0.015 0.020 0.030 0.040 0.060 0.080 0.1; do
+sbatch <<EOF
+#!/bin/sh
+#SBATCH -J cgna_${molarity}
+#SBATCH --output=cgna_${molarity}.out
+#SBATCH --error=cgna_${molarity}.err
+#SBATCH -t 01:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=48
+#SBATCH -A lu2025-2-42
 RUST_LOG="Debug" $HOME/.cargo/bin/duello \
     scan \
     -1 7ktz.xyz \
     -2 7ktz.xyz \
     --rmin 32 --rmax 60 --dr 1.0 \
     --top topology.yaml \
-    --resolution 0.5 \
+    --resolution 0.25 \
     --cutoff 1000 \
     --molarity ${molarity} \
     --temperature 300 \
     --pmf pmf_${molarity}.dat
+EOF
 done
